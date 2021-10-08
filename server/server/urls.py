9ed5834c5dc61c625f01899cca0632cd45696c30
resistanceJkee <maximkostenko123@gmail.com>
2021-10-08 13:06:02 +0400

    rest routing

--- server/server/urls.py
+++ server/server/urls.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
 from django.urls import path, include
 
+"""
+/api
+    GET /course
+    GET /course/id(type int)
+    POST /course
+    DELETE /course
+    PATCH/PUT /course
+    GET /parent
+    PATCH/PUT /parent
+    DELETE /parent (?)
+    GET /teacher
+    GET /schedule?id=type int
+/account 
+    POST /sign_in
+    POST /sign_up
+    POST /logout
+"""
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
