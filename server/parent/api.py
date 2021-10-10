276fba388ce4d23efee3c78f6160fb97e8e0d911
thebadfordota <62463640+thebadfordota@users.noreply.github.com>
2021-10-10 13:39:50 +0400

    add child create

--- server/parent/api.py
+++ server/parent/api.py
@@ -1,8 +1,9 @@
 from rest_framework import viewsets, permissions
-from rest_framework.views import APIView
 from .serializers import *
 from .models import *
 from accounts.models import AdvancedUser
+from courses.models import Courses, Lessons, Attendance
+from rest_framework.response import Response
 
 
 class ParentViewSet(viewsets.ModelViewSet):
@@ -17,6 +18,22 @@ class ChildViewSet(viewsets.ModelViewSet):
     queryset = Child.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = ChildSerializer
+
+    def create(self, request, *args, **kwargs):
+        child_data = request.data
+        curr_parent = AdvancedUser.objects.get(id=child_data["parent_of_child"])
+        curr_course = Courses.objects.get(id=child_data["courses"])
+        new_child = Child.objects.create(last_name=child_data["last_name"], first_name=child_data["first_name"],
+                                         patronymic=child_data["patronymic"],
+                                         parent_of_child=curr_parent, courses=curr_course)
+        new_child.save()
+        serializer = ChildSerializer(new_child)
+        all_lessons = Lessons.objects.filter(course=curr_course)
+        for one_lesson in all_lessons:
+            new_attend = Attendance.objects.create(child=new_child, lesson=one_lesson)
+            new_attend.save()
+        return Response(serializer.data)
+
 
     # def create(self, request, *args, **kwargs):
 
